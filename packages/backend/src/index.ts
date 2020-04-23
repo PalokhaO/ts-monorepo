91b9ae19817dc465fdfa250b3beb6de103fc11f7
Palokha Oleksandr <palokha.oa@gmail.com>
2020-04-23 11:45:57 +0300

    Added smplte backend

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+    origin: true,
+}));
+
+app.get('/hello', (req, res) => {
+    res.json({
+        hello: 'world'
+    });
+});
+
+app.listen(process.env.PORT || 5000);
